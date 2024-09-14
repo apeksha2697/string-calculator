f1d31a052636422f5f0695bcc918e1f04ecc065f
apeksha2697 <apekshasingh664@gmail.com>
2024-09-14 20:57:17 +0530

    added comments

--- test_string_calculator.rb
+++ test_string_calculator.rb
@@ -7,15 +7,18 @@ class StringCalculatorTest < Minitest::Test
     @calculator = StringCalculator.new
   end
 
+  # handle empty string
   def test_empty_string
     assert_equal 0, @calculator.add('')
   end
 
+  # handle 1 number
   def test_one_number_string
     assert_equal 1, @calculator.add('1')
   end
 
+  # handle as many numbers possible
   def test_two_number_string
-    assert_equal 11, @calculator.add('5,6')
+    assert_equal 29, @calculator.add('5,6,3,4,5,6')
   end 
 end
